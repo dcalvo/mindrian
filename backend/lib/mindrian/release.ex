54294cb33bcf1b50f0d950521fc2c856c6f86ba9
David Calvo <david@dcalvo.io>
2025-12-15 17:48:22 -0500

    add test accounts

--- backend/lib/mindrian/release.ex
+++ backend/lib/mindrian/release.ex
@@ -5,6 +5,66 @@ defmodule Mindrian.Release do
   """
   @app :mindrian
 
+  alias Mindrian.Accounts
+  alias Mindrian.Accounts.User
+  alias Mindrian.Repo
+
+  @test_accounts [
+    {"test1@mindrian.local", "testpassword1"},
+    {"test2@mindrian.local", "testpassword2"},
+    {"test3@mindrian.local", "testpassword3"}
+  ]
+
+  # TODO: To remove test accounts feature:
+  # 1. Delete test users in prod via remote console:
+  #      for email <- ["test1@mindrian.local", "test2@mindrian.local", "test3@mindrian.local"] do
+  #        Mindrian.Accounts.get_user_by_email(email) |> Mindrian.Repo.delete()
+  #      end
+  # 2. Remove test login buttons from lib/mindrian_web/controllers/user_session_html/new.html.heex
+  # 3. Remove this module's test account code and seeds.exs call
+
+  @doc """
+  Seeds test users. Use this in production via remote console.
+  """
+  def seed_test_users do
+    load_app()
+
+    {:ok, _, _} =
+      Ecto.Migrator.with_repo(Repo, fn _repo ->
+        seed_test_users!()
+      end)
+
+    :ok
+  end
+
+  @doc """
+  Seeds test users. Use this when repo is already started (e.g., seeds.exs).
+  """
+  def seed_test_users! do
+    for {email, password} <- @test_accounts do
+      case Accounts.get_user_by_email(email) do
+        nil -> create_test_user(email, password)
+        user -> update_test_user(user, password)
+      end
+    end
+  end
+
+  defp create_test_user(email, password) do
+    now = DateTime.utc_now(:second)
+
+    %User{}
+    |> User.email_changeset(%{email: email}, validate_unique: true)
+    |> User.password_changeset(%{password: password})
+    |> Ecto.Changeset.put_change(:confirmed_at, now)
+    |> Repo.insert!()
+  end
+
+  defp update_test_user(user, password) do
+    user
+    |> User.password_changeset(%{password: password})
+    |> Repo.update!()
+  end
+
   def migrate do
     load_app()
 
--- backend/priv/repo/seeds.exs
+++ backend/priv/repo/seeds.exs
@@ -9,3 +9,5 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Mindrian.Release.seed_test_users!()
